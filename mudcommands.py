069770682c05302e4f5cd0749aa3bb7c98a81807
Mutinifni <pratyushpatel.1995@gmail.com>
2014-04-14 08:16:56 +0530

    Learning to commit!

---
+++ mudcommands.py
@@ -0,0 +1,63 @@
+#Experiment with Git.
+#Test file for directions.
+
+class Room:
+	def __init__(self,dir):
+		self.exit = [dir]
+	def exits(self):
+		return self.exit
+
+def executecommand(command, currentRoom):
+	if command == 'w':
+		west()
+	elif command == 'e':
+		east()
+	elif command == 's':
+		south()
+	elif command == 'n':
+		currentRoom = north()
+	else:
+		print("What's that??")
+
+
+def directionexists(dir):
+	if dir in currentRoom.exits():
+		return True
+	else:
+		return False
+
+def west():
+	if(directionexists('W')):
+		print("You moved west.")
+	else:
+		print("You cannot go that way.")
+
+def east():
+	if(directionexists('E')):
+		print("You moved east.")
+	else:
+		print("You cannot go that way.")
+
+def south():
+	if(directionexists('S')):
+		print("You moved south.")
+	else:
+		print("You cannot go that way.")
+
+def north():
+	if(directionexists('N')):
+		print("You moved north.") # Something like this...
+	else:
+		print("You cannot go that way.")
+
+currentRoom = Room('N')
+
+state = 0 # For standing, sitting, sleeping or fighting.
+
+command = input("Enter command: ")
+while(command != 'exit'):
+	hp = 100
+	mv = 100
+	executecommand(command, currentRoom)
+	command = input("HP: %d || MV: %d || >" %(hp, mv))
+
