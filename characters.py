1dbfbce0fce10ea07169914b00ccb183b990b0e4
Civilwargeeky <civilwargeeky@yahoo.com>
2014-04-13 22:35:49 -0500

    Added a couple of examples

--- characters.py
+++ characters.py
@@ -1,1 +1,15 @@
 #This file will have classes and functions for characters. Including the player.
+
+class Character(object): #Example #Also class names are all upper case, not camelCase
+  def __init__(self, hp):
+    self.hp = hp
+    
+class Player(Character): #Example
+  def __init__(self, hp):
+    super.__init__(hp)
+    self.inventory = {}
+    
+class EnemyNPC(Character): #Example
+  def __init__(self, hp):
+    super.__init__(hp)
+    self.annoyingness = 9001
--- levels.py
+++ levels.py
@@ -1,2 +1,9 @@
 #This file will contain the classes and associated functions for getting information from rooms.
 #It will also deal with events and things that happen in rooms.
+
+import saving
+
+def Room(object): #Example
+  def __init__(self, stuff):
+    if saving.exists(stuff):
+      self = saving.load(stuff)
